d4792ad21a940d17bf7e97d94c0830e3335173b8
Stefan Rotaru <stefanrotaru@hotmail.com>
2022-03-28 11:08:02 +0300

    fixed indentation

--- script.js
+++ script.js
@@ -5,57 +5,41 @@ var body = document.getElementById("gradient");
 var button = document.querySelector("button");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-	// Adding the new colors to the button's background too!
-	button.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value
-	+ ", " 
-	+ color2.value
-	+ ")";
+  // Adding the new colors to the button's background too!
+  button.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-	css.textContent = body.style.background + ";";
-};
+  css.textContent = body.style.background + ";";
+}
 
 // This function creates random colors (grabbed it from stackoverflow).
 // Appears to be 'randomly'(pun intended) failing. Maybe someone can help?
 // (fail example: The specified value "#f7dcc" does not conform to the required format.)
 // Fixed: replaced |Math.floor(Math.random() * 16777215).toString(16)| with |Math.random().toString(16).slice(2,8)|
 function randomColor() {
-	var newColor1 = '#'+ Math.random().toString(16).slice(2,8);
-	var newColor2 = '#'+ Math.random().toString(16).slice(2,8);
-	
-	// Adding the random colors to the input value 
-	color1.value = newColor1;
-	color2.value = newColor2;
+  var newColor1 = "#" + Math.random().toString(16).slice(2, 8);
+  var newColor2 = "#" + Math.random().toString(16).slice(2, 8);
 
-	// Adding the random colors to the background
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ newColor1 
-	+ ", " 
-	+ newColor2
-	+ ")";
-	
-	// Wanted to set the new colors to show on the button as well, so
-	// here it is!
-	button.style.background = 
-	"linear-gradient(to right, " 
-	+ newColor1 
-	+ ", " 
-	+ newColor2
-	+ ")";
+  // Adding the random colors to the input value
+  color1.value = newColor1;
+  color2.value = newColor2;
 
-	css.textContent = body.style.background + ";";
-};
+  // Adding the random colors to the background
+  body.style.background =
+    "linear-gradient(to right, " + newColor1 + ", " + newColor2 + ")";
 
-body.onload = setGradient()
+  // Wanted to set the new colors to show on the button as well, so
+  // here it is!
+  button.style.background =
+    "linear-gradient(to right, " + newColor1 + ", " + newColor2 + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+body.onload = setGradient();
 
 color1.addEventListener("input", setGradient);
 
